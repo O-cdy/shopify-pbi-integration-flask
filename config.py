be23ee2d0290f2eb2e5469acf190bfbb23622792
O-cdy <65551498+O-cdy@users.noreply.github.com>
2025-05-26 18:31:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
-import os
+# import os
+# from datetime import datetime
+# SHOPIFY_DOMAIN = os.environ.get("SHOPIFY_DOMAIN")
+# ACCESS_TOKEN = os.environ.get("SHOPIFY_TOKEN")
+# API_VERSION = os.environ.get("API_VERSION")
+# START_DATE = os.environ.get("START_DATE")
+# END_DATE = datetime.utcnow().strftime("%Y-%m-%dT23:59:59Z")
+
+
 from datetime import datetime
-SHOPIFY_DOMAIN = os.environ.get("SHOPIFY_DOMAIN")
-ACCESS_TOKEN = os.environ.get("SHOPIFY_TOKEN")
-API_VERSION = os.environ.get("API_VERSION")
-START_DATE = os.environ.get("START_DATE")
+
+SHOPIFY_DOMAIN = "homatics-tv.myshopify.com"
+ACCESS_TOKEN = "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"
+API_VERSION = "2023-10"
+START_DATE = "2025-04-01"
 END_DATE = datetime.utcnow().strftime("%Y-%m-%dT23:59:59Z")
-
-
